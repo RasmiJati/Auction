d2359e134c0c0634b1ca185b88b03c10a1aac6e7
Rasmi Jati <jatirasmi65@gmail.com>
2024-03-06 14:23:26 +0545

    model for Listing, bid and comment

---
+++ Models/Listing.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Auction.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }    
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImagePath { get; set; }
+        public bool IsSold { get; set; }
+        
+        [Required]
+        public string? IdentityUserId { get; set; }
+        [ForeignKey("IdentityUserId")]
+        
+        public IdentityUser? User {  get; set; }
+       
+        public List<Bid>? Bids { get; set; }  //one to many relationship   i.e. 1 listing many bids
+        public List<Comment>? Comments { get; set; }  //one to manay relationship
+    }
+}
